112047dc44a333233a34fd08e131ffee2f24c58c
Vidhyasagar <vidhyasagarmyana9598@gmail.com>
2025-06-24 15:31:37 +0530

    added backend deployed url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     setResult("");
 
     try {
-      const res = await axios.post("http://localhost:5000/api/scan", {
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/scan`, {
         resumeText,
       });
       setResult(res.data.result);
